cdc2c58d1194e7cd3438882bb61b732036768c34
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2017-07-28 21:06:18 +0400

    Added more server methods.

--- Sources/Server+Methods.swift
+++ Sources/Server+Methods.swift
@@ -14,4 +14,32 @@ extension Server {
     public func post(_ path: String, handler: @escaping RouteHandler) {
         custom("POST", path, handler: handler)
     }
+    
+    public func head(_ path: String, handler: @escaping RouteHandler) {
+        custom("HEAD", path, handler: handler)
+    }
+    
+    public func put(_ path: String, handler: @escaping RouteHandler) {
+        custom("PUT", path, handler: handler)
+    }
+    
+    public func patch(_ path: String, handler: @escaping RouteHandler) {
+        custom("PATCH", path, handler: handler)
+    }
+    
+    public func delete(_ path: String, handler: @escaping RouteHandler) {
+        custom("DELETE", path, handler: handler)
+    }
+    
+    public func trace(_ path: String, handler: @escaping RouteHandler) {
+        custom("TRACE", path, handler: handler)
+    }
+    
+    public func connect(_ path: String, handler: @escaping RouteHandler) {
+        custom("CONNECT", path, handler: handler)
+    }
+    
+    public func options(_ path: String, handler: @escaping RouteHandler) {
+        custom("OPTIONS", path, handler: handler)
+    }
 }
