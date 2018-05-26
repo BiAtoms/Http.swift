1ecb020664cf67248a334b208f7d2de6da187274
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2018-05-26 15:49:29 +0400

    Updated SMP tools to 4.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,25 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "HttpSwift",
+    products: [
+        .library(
+            name: "HttpSwift",
+            targets: ["HttpSwift"]),
+    ],
     dependencies: [
-            .Package(url: "https://github.com/BiAtoms/Socket.swift.git", majorVersion: 2, minor: 2),
-            .Package(url: "https://github.com/BiAtoms/Request.swift.git", majorVersion: 2, minor: 1), // for tests
-        ]
+        .package(url: "https://github.com/BiAtoms/Socket.swift.git", from: "2.2.0"),
+        .package(url: "https://github.com/BiAtoms/Request.swift.git", from: "2.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "HttpSwift",
+            dependencies: ["SocketSwift"],
+            path: "Sources"),
+        .testTarget(
+            name: "HttpSwiftTests",
+            dependencies: ["HttpSwift", "RequestSwift"]),
+    ]
 )
